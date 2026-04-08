ea7b83f491c0fe1e9786e1f5a9e1644fce9cf07a
Madandola james <jamesmadandola@gmail.com>
2026-04-08 12:21:14 +0100

    fix cors

--- index.js
+++ index.js
@@ -7,35 +7,35 @@ import connectDb from "./config/Mongodb.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import ProfileRoutes from "./routes/profileRoutes.js";
 import mentorRoutes from "./routes/mentorRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
-
 app.use(cors({       
-    origin:[ "https://mentorship-frontend-5vsz.vercel.app",  "https://mentorship-frontend-ho2c.vercel.app/","http://localhost:5173/"],
-    credentials:true,
-    methods:["GET","PUT","DELETE","POST"],
-    allowedHeaders:["content-type","Authorization"]
- }))
-app.use("/api/auth",AuthRoutes);
-app.use("/api/profile", ProfileRoutes)
-app.use("/api/", mentorRoutes)
-
-
-app.get("/", (req, res)=>{
-    res.json({message: " welcome to backend "})
-})
+  origin: [
+    "https://mentorship-frontend-5vsz.vercel.app",
+    "https://mentorship-frontend-ho2c.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+  methods: ["GET", "PUT", "DELETE", "POST"],
+  allowedHeaders: ["content-type", "Authorization"]
+}))
 
 app.use(cookieParser());
-
-
 app.use(express.json());
+
+app.use("/api/auth", AuthRoutes);
+app.use("/api/profile", ProfileRoutes);
+app.use("/api/", mentorRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to backend" })
+})
 
 connectDb();
 
-
-
-app.listen(8000,()=> {
-    console.log("server is running on http://localhost:8000");
+app.listen(8000, () => {
+  console.log("server is running on http://localhost:8000");
 });
