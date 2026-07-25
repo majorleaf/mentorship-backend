1df1fbf8d46f1beb51e72e655d8b30e7304a17af
Madandola james <jamesmadandola@gmail.com>
2026-07-25 19:37:16 +0100

    yolo

--- models/userSchema.js
+++ models/userSchema.js
@@ -7,4 +7,41 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true},
     
-})
+    
+    accountType: {
+        type: String,
+        enum: ['b2c', 'b2b'],
+        default: 'b2c'
+    },
+    organizationId: {
+        type: String,
+        enum: ['mentee', 'mentor', 'hr_admin', 'super_admin'],
+        default: 'mentee'
+    },
+
+    // matching profile data 
+    profile: {
+        bio: {
+            type: String, maxLength: 500
+        },
+        skills: {type:  String, trim: true },
+        goals: { type: String, trim: true },
+    },
+
+    // mentorPrefernces // if role === mentor
+    mentorPrefernces: {
+        isAvailable: { type: Boolean, default: false },
+        maxMentees: { type: Number, default: 3 },
+        topics: [{ type: String }]
+    }
+}, { timestamps: true });
+
+// Indexing for performance
+// In order to make searching for mentors under specific enterprise fast
+userSchema.index({ organizationId: 1, role: 1 });
+userSchema.index({ email: 1});
+
+const User = mongoose.models.users || mongoose.model('User', userSchema);
+
+export default User;
+
