728f56e4e5a671c1835472f1c38a4af012b1b08d
Madandola james <jamesmadandola@gmail.com>
2026-07-25 21:35:09 +0100

    updated the new unified model

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,36 @@
 import jwt from "jsonwebtoken"
-import AuthModel from "../models/authSchema.js"
+import User from "../models/userSchema.js";
 
-const authMiddleware = async (req , res ,next )=>{
+const authMiddleware = async (req , res ,next ) => {
     try{
-        const token = req.cookies.token
+        const token = req.cookies.token;
+
         if(!token){
             return res.status(401).json({message: "unauthorized access, no tken found "})
         }
 
-        let decodedToken
+        let decodedToken;
         try{
             decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
         } catch(error) {    
             console.log(error)
             return res.status(401).json({message: "invalid token or token has expired"})
          }
-        const user = await AuthModel.findById(decodedToken.id)
+         // updated the new unified User model and explicitly exclude the password field
+        const user = await User.findById(decodedToken.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User account no longer exists"});
+        }
+        // Attached the user document to the request content
         req.user = user
-        next()
+
+        next();
     }catch (error) {
+        console.error("Auth Middleware error:", error );
+        return res.status(500).json({ message: " Server error during authentication"});
 
     } 
-}
+};
+
 export default authMiddleware;
